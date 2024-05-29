5d5627aeba4435bdf03bb75a68c3a6d2e4615b53
Hendrik <161131951+brownhamer@users.noreply.github.com>
2024-05-29 13:34:49 +0200

    Back into random number specs from game rules

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 let theirNumber = 0
 let halfSecond = 500000
-let myNumber = randint(0, 2)
+let myNumber = randint(0, 9)
 theirNumber = -1
 radio.setGroup(1)
 loops.everyInterval(1000, function () {
@@ -14,7 +14,7 @@ loops.everyInterval(1000, function () {
     } else {
         pins.analogWritePin(AnalogPin.P13, 1023)
         pins.analogWritePin(AnalogPin.P14, 0)
-        myNumber = randint(0, 2)
+        myNumber = randint(0, 9)
     }
     basic.showNumber(myNumber)
     radio.sendNumber(myNumber)
